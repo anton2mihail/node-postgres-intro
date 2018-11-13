0959712299d79dee39aeef272e65a624b25dd5db
Anton Mihail Lachmaniucu <antonmihail@gmail.com>
2018-11-13 11:37:28 -0500

    Database migrations added

---
+++ knexfile.js
@@ -0,0 +1,42 @@
+// Update with your config settings.
+const settings = require('./settings.json');
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: `postgres://${settings.user}:${settings.password}@${settings.hostname}:${settings.port}/${settings.database}`
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
